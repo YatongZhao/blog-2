adc22318fe78c49841f63777c212978004dcdfd6
yatongzhao <yatongzhao@sohu-inc.com>
2018-05-28 11:40:54 +0800

    update assetsPublicPath

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/dist/',
+    assetsPublicPath: '/blog-2/dist/',
 
     /**
      * Source Maps
