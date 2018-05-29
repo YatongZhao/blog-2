d208b473a8352e33f909b0cc474178bdf21701b8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-29 03:01:17 +0000

    Deploy zyt76/blog-2 to github.com/zyt76/blog-2.git:gh-pages

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,4 +2,4 @@
 source /etc/profile
 source /root/.bash_profile
 export SSHPASS=$DEPLOY_PASS
-sshpass -e scp -o stricthostkeychecking=no -r ./src* $DEPLOY_USER@47.52.171.181:/opt/
+sshpass -e scp -o stricthostkeychecking=no -r ./src/* $DEPLOY_USER@47.52.171.181:/opt/
