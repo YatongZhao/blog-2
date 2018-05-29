c2a3ff8eb62f6c2b124c46256dd1eae778a5a143
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-29 08:13:54 +0000

    Deploy zyt76/blog-2 to github.com/zyt76/blog-2.git:gh-pages

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source /etc/profile
-source /root/.bash_profile
-export SSHPASS=$DEPLOY_PASS
-sshpass -e scp -o stricthostkeychecking=no -r ./src/* $DEPLOY_USER@47.52.171.181:/opt/
+
+cd /opt/blog-2
+git pull origin gh-pages
+echo "DEPLOY successfully..."
