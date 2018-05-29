c6bbfe84e1f82f48f57893f5157df1f2b66445c3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-29 12:59:47 +0000

    Deploy zyt76/blog-2 to github.com/zyt76/blog-2.git:gh-pages

---
+++ src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <div class="header">Home Page</div>
+    <div class="container">
+      <dl class="skill-list">
+        <dt>skill</dt>
+        <dd>Git</dd>
+        <dd>webpack</dd>
+        <dd>Vue</dd>
+        <dd>HTTP</dd>
+        <dd>HTML</dd>
+        <dd>CSS</dd>
+        <dd>JavaScript</dd>
+        <dd>Node.js</dd>
+      </dl>
+      <div class="category">
+        <div>mastered</div>
+        <div>not-mastered</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+  .header {
+
+  }
+  .container {
+    display: flex;
+    .skill-list {
+
+    }
+    .category {
+      display: flex;
+    }
+  }
+}
+</style>
